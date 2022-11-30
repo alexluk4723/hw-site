d2b0a8ee66ee389c05e90b468eb3ea00a0c09d3a
alexluk4723 <sasha-lukyanov-02@mail.ru>
2022-11-30 14:19:07 +0300

    refactor prepod.php

--- prepod.php
+++ prepod.php
@@ -1,17 +1,26 @@
-
+<html>
+<head>
+    <meta charset="UTF-8">
+    <style>
+        .dz {
+            border: 1px solid red;
+        }
+    </style>
+</head>
+</html>
 <?php
-if($_GET["key"]!="lol"){
-    exit;
-}
+//if ($_GET["key"] != "lol") {
+//    exit;
+//}
 $dbh = new PDO('mysql:host=85.193.80.157; port=3306; dbname=default_db', 'gen_user', 'Qazwsx23e47');
-$arr = array();
-foreach($dbh->query("SELECT * FROM `homeworks` INNER JOIN `users_hw` ON 
-`homeworks`.`id_user`=`users_hw`.`id`") as $row){
-echo '
-<div>
-    Имя: '. $row["name"] .'<br>
-    ID дз: '. $row["id_hw"] .'<br>
-    Файл: <a href="'. $row["file"] .'">Ссылка</a>
-</div><br>';
-echo '------------------------------------------------<br>';
+foreach ($dbh->query("SELECT * FROM `homeworks` INNER JOIN `users_hw` ON 
+`homeworks`.`id_user`=`users_hw`.`id`") as $row) {
+    ?>
+<div class="dz">
+    Имя: <?php echo $row["name"];  ?> <br>
+    ID дз: <?php echo $row["id_hw"];  ?> <br>
+    Файл: <a href="<?php echo $row['file']; ?>">Ссылка на скачивание</a>
+</div>
+<?php
 }
+?>
